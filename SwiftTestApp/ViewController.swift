06808ef9df43390c6429338c3111b221353a58ce
Christian Wasmer <christian.wasmer@gmail.com>
2017-02-23 22:20:54 +0100

    My first commit!

--- SwiftTestApp/ViewController.swift
+++ SwiftTestApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func ButtonTapped(_ sender: Any) {
+        tapCount = tapCount + 1
+        print(tapCount)
+        if tapCount >= 10 {
+            Label.text = "More than 10 times!!!"
+        }
+
+    }
+    @IBAction func CoolioTapped(_ sender: Any) {
+        Label.text = "Buttons are cool!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
